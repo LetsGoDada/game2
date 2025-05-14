9a543bfea58f102b2f102646a5eceef801f26093
LetsGoDada <DadaK20@outlook.de>
2025-05-14 08:42:27 +0200

    Aktualisiere pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts, main.py, tilemap.g.jres, tilemap.g.ts

--- main.py
+++ main.py
@@ -1,6 +1,42 @@
 @namespace
 class SpriteKind:
     Gegner = SpriteKind.create()
+    Baer = SpriteKind.create()
+    Hintergrund = SpriteKind.create()
+
+def on_a_pressed():
+    animation.run_image_animation(mySprite2,
+        assets.animation("""
+            Ani1
+            """),
+        100,
+        False)
+controller.A.on_event(ControllerButtonEvent.PRESSED, on_a_pressed)
+
+mySprite2: Sprite = None
+music.play(music.create_song(assets.song("""
+        melodie
+        """)),
+    music.PlaybackMode.LOOPING_IN_BACKGROUND)
+mySprite3 = sprites.create(assets.image("""
+        Hintergrund
+        """),
+    SpriteKind.Hintergrund)
+scaling.scale_by_pixels(mySprite3,
+    121,
+    ScaleDirection.HORIZONTALLY,
+    ScaleAnchor.MIDDLE,
+    True)
+scaling.scale_by_pixels(mySprite3,
+    50,
+    ScaleDirection.VERTICALLY,
+    ScaleAnchor.MIDDLE,
+    True)
 mySprite = sprites.create(assets.image("""
     Bär
-    """), SpriteKind.Gegner)
+    """), SpriteKind.Baer)
+mySprite.set_position(150, 68)
+mySprite2 = sprites.create(assets.image("""
+    Player
+    """), SpriteKind.player)
+controller.move_sprite(mySprite2, 100, 100)
--- main.ts
+++ main.ts
@@ -3,11 +3,30 @@ namespace SpriteKind {
     export const Baer = SpriteKind.create()
     export const Hintergrund = SpriteKind.create()
 }
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    mySprite2,
+    assets.animation`Ani1`,
+    100,
+    false
+    )
+    animation.runMovementAnimation(
+    mySprite,
+    animation.animationPresets(animation.flyToCenter),
+    2000,
+    false
+    )
+})
+let mySprite2: Sprite = null
+let mySprite: Sprite = null
 music.play(music.createSong(assets.song`melodie`), music.PlaybackMode.LoopingInBackground)
+let statusbar = statusbars.create(20, 4, StatusBarKind.Health)
+statusbar.setPosition(80, 40)
 let mySprite3 = sprites.create(assets.image`Hintergrund`, SpriteKind.Hintergrund)
 scaling.scaleByPixels(mySprite3, 121, ScaleDirection.Horizontally, ScaleAnchor.Middle, true)
-scaling.scaleByPixels(mySprite3, 50, ScaleDirection.Vertically, ScaleAnchor.Middle, true)
-let mySprite = sprites.create(assets.image`Bär`, SpriteKind.Baer)
+scaling.scaleByPixels(mySprite3, 30, ScaleDirection.Vertically, ScaleAnchor.Middle, true)
+mySprite = sprites.create(assets.image`Bär`, SpriteKind.Baer)
 mySprite.setPosition(150, 68)
-let mySprite2 = sprites.create(assets.image`Player`, SpriteKind.Player)
+mySprite2 = sprites.create(assets.image`Player`, SpriteKind.Player)
+scene.cameraFollowSprite(mySprite2)
 controller.moveSprite(mySprite2, 100, 100)
